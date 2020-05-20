aa305a23dd1ef3a3ffba21689780ae1d684260e5
DavidMFern <43940445+DavidMFern@users.noreply.github.com>
2020-05-20 12:19:15 +0200

    errores en scripts

--- Assets/Script/ParalajeLayer.cs
+++ Assets/Script/ParalajeLayer.cs
@@ -2,12 +2,21 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
+public class ParalajeLayer : MonoBehaviour
 {
+    [SerializeField]
+    int layer = 0;
+    [SerializeField]
+    GameObject obj;
     // Start is called before the first frame update
+    
     void Start()
     {
+        obj = gameObject;
+        ParalajeMaster paralajeMaster = new ParalajeMaster();
         
+        paralajeMaster.AddLayer(layer, gameObject);
+
     }
 
     // Update is called once per frame
--- Assets/Script/ParalajeMaster.cs
+++ Assets/Script/ParalajeMaster.cs
@@ -4,15 +4,30 @@ using UnityEngine;
 
 public class ParalajeMaster : MonoBehaviour
 {
+    public GameObject test;
+
+    int temporalNumeroDeCapa;
+    GameObject temporalObjeto;
+
+    [SerializeField]
+    List<GameObject> layers= new List<GameObject>(new GameObject[50]);
     // Start is called before the first frame update
     void Start()
     {
-        
+        //layers.Insert(temporalNumeroDeCapa, temporalObjeto);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
+
+
+    public void AddLayer(int NumeroDeCapa, GameObject Objeto)
+    {
+        layers.Insert(NumeroDeCapa, Objeto);
+        print("dfvuge");
+    }
+
 }
