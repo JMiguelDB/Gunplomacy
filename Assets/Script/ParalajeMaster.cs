848861e26e65f2b56ab5eb7c5946fdf9b221696e
DavidMFern <43940445+DavidMFern@users.noreply.github.com>
2020-05-21 12:10:04 +0200

    Haora con el script paralaje podemos controlar un cubo segun la posicion del ratón.

--- Assets/Script/ParalajeMaster.cs
+++ Assets/Script/ParalajeMaster.cs
@@ -4,13 +4,19 @@ using UnityEngine;
 
 public class ParalajeMaster : MonoBehaviour
 {
-    public GameObject test;
-
-    int temporalNumeroDeCapa;
-    GameObject temporalObjeto;
-
+    public bool limpiarLista=false;
     [SerializeField]
     List<GameObject> layers= new List<GameObject>(new GameObject[50]);
+
+
+
+    Camera cam;
+    public Vector3 wp;
+
+
+
+
+
     // Start is called before the first frame update
     void Start()
     {
@@ -20,9 +26,56 @@ public class ParalajeMaster : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        //h
+        ObtenerPosicionRaton();
+
+
+
+        if (limpiarLista == true)
+        {
+            LimpiarLista();
+            limpiarLista = false;
+        }
+        Paralaje();
+
+
+
+
 
     }
 
+    void Paralaje()
+    {
+        for (int t = 0; t < layers.Count; t++)
+        {
+            if(layers[t] == null)
+            {
+                print("La posicion en lista " + t + " está vacia.");
+
+            }
+            else
+            {
+                print("Encontrado en la fila " + t + " el objeto " + layers[t]);
+                layers[t].transform.position += new Vector3(wp.x, wp.y,0) * t;
+            }
+        }
+
+    }
+
+
+
+    void ObtenerPosicionRaton()
+    {
+        cam = Camera.main;
+        wp = cam.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 8));
+    }
+
+    //[i] Añadir a los script para cada cambio de escena.
+    public void LimpiarLista()
+    {
+        layers.Clear();
+        layers.AddRange(new GameObject[50]);
+    }
 
     public void AddLayer(int NumeroDeCapa, GameObject Objeto)
     {
