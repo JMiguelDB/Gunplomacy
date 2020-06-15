2fb7b2aaca84d1386302e6fd6038e3d959e6c8a9
DavidMFern <43940445+DavidMFern@users.noreply.github.com>
2020-06-16 01:40:02 +0200

    Función recojer arma Android, Linternas , y adaptados algunos códigos.

---
+++ Assets/Script/UI/Joystick/BotonMultyTactilCambio.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class BotonMultyTactilCambio : MonoBehaviour, IPointerUpHandler, IPointerDownHandler
+{
+    [HideInInspector]
+    public bool Activar;
+
+
+    public void OnPointerUp(PointerEventData Datos)
+    {
+        Activar = false;
+    }
+
+    public void OnPointerDown(PointerEventData Datos)
+    {
+        Activar = true;
+    }
+
+}
