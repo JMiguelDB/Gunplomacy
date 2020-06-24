1571f9694357c6c9959ca0ac241cc5edabfdd3aa
DavidMFern <43940445+DavidMFern@users.noreply.github.com>
2020-06-24 07:05:41 +0200

    Puerta con Animación , Nuevo tile de marco de puerta, script abrir, y coliders testeados y verificados a fondo.

---
+++ Assets/Script/Puertas/AbrirPuerta.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AbrirPuerta : MonoBehaviour
+{
+    Animator animator;
+    private void Start()
+    {
+        animator = gameObject.GetComponent<Animator>();
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        animator.SetBool("Abrir", true);
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        animator.SetBool("Abrir", false);
+    }
+}
